acb5de1d28c97e473094e62d62e842533fe72bf6
QuadieZ <62594692+QuadieZ@users.noreply.github.com>
2020-06-11 18:52:11 +0700

    Add files via upload

---
+++ eventstool/hi.js
@@ -0,0 +1,38 @@
+function getSum(num1,num2) {
+    let num3 = num1 + num2;
+    console.log(num3)
+}
+
+getSum(2,3);
+
+const Sum = (num1,num2) => {
+    console.log(num1 + num2);
+}
+
+Sum(2,2)
+
+let x = 4;
+
+if (x == 2) {
+    console.log(x)
+} else if (x == 3) {
+    console.log('yay')
+} else {
+    console.log('no')
+}
+
+let array = [1,2,3,4]
+
+const user = {
+    'name': 'Naat',
+    'hp': 20
+}
+
+
+const Add = (num1,num2) => {
+    const num3 = num1 + num2
+}
+
+const num = Add(2,3);
+console.log(num)
+
